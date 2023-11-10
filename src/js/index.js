aceea80249c05312c62f6e52f3f008cb80564738
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-10 20:25:36 +0000

    LightFrame work in progress

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import LightsFrame from "./component/LightsFrame.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<LightsFrame />, document.querySelector("#app"));
