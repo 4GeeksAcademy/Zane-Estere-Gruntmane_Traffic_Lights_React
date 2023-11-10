23a90afd9461beefe3b7bbf211af719bbedfba44
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-10 21:50:49 +0000

    lights work in progress

--- src/js/component/LightsFrame.jsx
+++ src/js/component/LightsFrame.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
+import "./styles/index.css";
 
 const LightsFrame = () => {
   return (
+    <div className="container-fluid col-sm-1 col-md-1 col-lg-1">
+      
+      <div className="verticalLine"> </div>
 
-    <div className="container-fluid  col-sm-1 mt-5">
-      <div className="p-2"> flex 1 </div>
-
-      <div className="Lights bg-black border rounded text-center">
+      <div className="wrapper bg-black border rounded text-center">
         <div className="redLight px-2 py-2">
           <i className="fa-solid fa-circle fa-2xl" style={{ color: "#cc1515" }}></i>
         </div>
-        <div className="px-2 py-2">
+        <div className="yellowLight px-2 py-2">
           <i className="fa-solid fa-circle fa-2xl" style={{ color: "#f6fa0d" }}></i>
         </div>
-        <div className="px-2 py-2">
-          <i className="fa-solid fa-circle fa-2xl" style={{ color: "#38f00f" }}></i> </div>
+        <div className="greenLight px-2 py-2">
+          <i className="fa-solid fa-circle fa-2xl" style={{ color: "#38f00f" }}></i>
+        </div>
       </div>
     </div>
   );
