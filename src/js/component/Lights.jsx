6bd35ff06c0fcb3dde4c71fe6697f25ebe42f81b
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-13 21:07:29 +0000

    traffic lights funcion still needs to find solution for positioning

--- src/js/component/Lights.jsx
+++ src/js/component/Lights.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const Lights = ({redLight, yellowLight, greenLight, onClickRed, onClickYellow, onClickGreen}) => {
-
-  let red = <i className="sign fa-solid fa-circle fa-2xl" style={{ color: "#cc1515" }}></i>;
-  let yellow = <i className="sign fa-solid fa-circle fa-2xl" style={{ color: "#f6fa0d" }}></i>;
-  let green = <i className="sign fa-solid fa-circle fa-2xl" style={{ color: "#38f00f" }}></i>;
+const Lights = ({ redLight, yellowLight, greenLight, onClickRed, onClickYellow, onClickGreen }) => {
+  let red = <i className={`sign ${redLight} fa-solid fa-circle fa-2xl`} style={{ color: "#cc1515" }}></i>;
+  let yellow = <i className={`sign ${yellowLight} fa-solid fa-circle fa-2xl`} style={{ color: "#f6fa0d" }}></i>;
+  let green = <i className={`sign ${greenLight} fa-solid fa-circle fa-2xl`} style={{ color: "#38f00f" }}></i>;
 
   return (
-    <div className="d-flex flex-row col-sm-6 col-md-6 col-lg-6 ">
+    
+    <div className="container-fluid d-flex flex-column align-self-center col-sm-6 col-md-6 col-lg-6">
       <div className="trafficHold col-sm-1 col-md-1 col-lg-1"></div>
       <div className="wrapper col-sm-1 col-md-1 col-lg-1">
-        <div className={`redLight ${redLight}`} onClick={onClickRed}> {red} </div>
-        <div className={`yellowLight ${yellowLight}`} onClick={onClickYellow}> {yellow} </div>
-        <div className={`greenLight ${greenLight}`} onClick={onClickGreen}> {green} </div>
+        <div className="redLight" onClick={onClickRed}>{red}</div>
+        <div className="yellowLight" onClick={onClickYellow}>{yellow}</div>
+        <div className="greenLight" onClick={onClickGreen}>{green}</div>
       </div>
     </div>
   );
