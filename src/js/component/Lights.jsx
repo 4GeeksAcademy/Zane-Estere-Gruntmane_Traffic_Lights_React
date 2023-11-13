80c8536919d59938f35a2ad85d84a801e9b724b5
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-13 20:27:22 +0000

    traffic light function solved details needs to be ajusted

--- src/js/component/Lights.jsx
+++ src/js/component/Lights.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Lights = () => {
+const Lights = ({redLight, yellowLight, greenLight, onClickRed, onClickYellow, onClickGreen}) => {
 
   let red = <i className="sign fa-solid fa-circle fa-2xl" style={{ color: "#cc1515" }}></i>;
   let yellow = <i className="sign fa-solid fa-circle fa-2xl" style={{ color: "#f6fa0d" }}></i>;
@@ -10,9 +10,9 @@ const Lights = () => {
     <div className="d-flex flex-row col-sm-6 col-md-6 col-lg-6 ">
       <div className="trafficHold col-sm-1 col-md-1 col-lg-1"></div>
       <div className="wrapper col-sm-1 col-md-1 col-lg-1">
-        <div className="redLight"> {red} </div>
-        <div className="yellowLight"> {yellow} </div>
-        <div className="greenLight"> {green} </div>
+        <div className={`redLight ${redLight}`} onClick={onClickRed}> {red} </div>
+        <div className={`yellowLight ${yellowLight}`} onClick={onClickYellow}> {yellow} </div>
+        <div className={`greenLight ${greenLight}`} onClick={onClickGreen}> {green} </div>
       </div>
     </div>
   );
