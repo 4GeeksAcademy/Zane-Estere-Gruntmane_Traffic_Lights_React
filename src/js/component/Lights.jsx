9a17673e686e50870e7516442567e2d4f53a9611
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-13 13:42:21 +0000

    traffic lights style submit

--- src/js/component/Lights.jsx
+++ src/js/component/Lights.jsx
@@ -7,9 +7,9 @@ const Lights = () => {
   let elementGreen = <i className="fa-solid fa-circle fa-2xl" style={{ color: "#38f00f" }}></i>;
 
   return (
-    <div className="trafficLight container-fluid col-sm-6">
-      <div className="trafficHold"></div>
-      <div className="wrapper col-6 ">
+    <div className="trafficLight d-flex flex-row ">
+      <div className="trafficHold col-sm-1 col-md-1 col-lg-1"></div>
+      <div className="wrapper col-sm-1 col-md-1 col-lg-1">
         <div className="redLight"> {elementRed} </div>
         <div className="yellowLight"> {elementYellow} </div>
         <div className="greenLight"> {elementGreen} </div>
